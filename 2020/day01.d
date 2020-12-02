11beabad07e9697f2861811b2da1fabae5f7b866
Joshua Hodkinson <joshua.hodkinson@dominos.com.au>
2020-12-02 11:52:04 +1000

    [DAY-01B]

--- 2020/day01.d
+++ 2020/day01.d
@@ -5,7 +5,7 @@ import std.algorithm : reduce;
 void main()
 {
 	const entries = readInput();
-	writeln(entries.sumTo2020().reduce!((a, b) => a * b));
+	writeln(entries.sum3To2020().reduce!((a, b) => a * b));
 }
 
 uint[2] sumTo2020(const uint[] entries)
@@ -26,8 +26,31 @@ uint[2] sumTo2020(const uint[] entries)
 unittest
 {
 	const entries = sumTo2020([1721, 979, 366, 299, 675, 1456]);
-	writeln(entries);
 	assert(entries == [1721, 299]);
+}
+
+uint[3] sum3To2020(const uint[] entries)
+{
+	foreach (entry1; entries)
+	{
+		foreach (entry2; entries)
+		{
+			foreach (entry3; entries)
+			{
+				if (entry1 + entry2 + entry3 == 2020)
+				{
+					return [entry1, entry2, entry3];
+				}
+			}
+		}
+	}
+	assert(false);
+}
+
+unittest
+{
+	const entries = sum3To2020([1721, 979, 366, 299, 675, 1456]);
+	assert(entries == [979, 366, 675]);
 }
 
 uint[] readInput()
