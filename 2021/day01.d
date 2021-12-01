b9205327bf1758b35a5f4cfcd0588416e994c1bd
Joshua Hodkinson <joshua.hodkinson@thoughtworks.com>
2021-12-01 16:37:34 +1000

    [D01A]

--- 2021/day01.d
+++ 2021/day01.d
@@ -1,11 +1,30 @@
+import std.algorithm;
+import std.conv;
+import std.range;
 import std.stdio;
+import std.string;
+import std.typecons;
 
 void main()
 {
-	writeln("Day 01...");
+	File("day01.in", "r").byLine.map!(to!uint).array.countDepthIncreased.writeln;
+}
+
+uint countDepthIncreased(in uint[] depths)
+{
+	return zip(depths, depths
+			.dropOne).map!(a => a[0] < a[1])
+		.map!(to!uint)
+		.reduce!"a + b";
 }
 
 unittest
 {
-	assert(true);
+	const uint[] depths = [
+		199, 200, 208, 210, 200, 207, 240, 269, 260, 263
+	];
+
+	const count = countDepthIncreased(depths);
+
+	assert(count == 7, format!"Expected 7, received: %s"(count));
 }
