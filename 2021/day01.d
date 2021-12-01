ed7393f1e776164aa237aea8f74007dcbad6bb31
Joshua Hodkinson <joshua.hodkinson@thoughtworks.com>
2021-12-01 16:53:52 +1000

    [D01B]

--- 2021/day01.d
+++ 2021/day01.d
@@ -7,13 +7,15 @@ import std.typecons;
 
 void main()
 {
-	File("day01.in", "r").byLine.map!(to!uint).array.countDepthIncreased.writeln;
+	const depths = File("day01.in", "r").byLine.map!(to!uint).array;
+	depths.countPairsIncreasing.writeln;
+	depths.countTripletsIncreasing.writeln;
 }
 
-uint countDepthIncreased(in uint[] depths)
+uint countPairsIncreasing(in uint[] depths)
 {
-	return zip(depths, depths
-			.dropOne).map!(a => a[0] < a[1])
+	return depths.pairwise
+		.map!(a => a[0] < a[1])
 		.map!(to!uint)
 		.reduce!"a + b";
 }
@@ -23,8 +25,36 @@ unittest
 	const uint[] depths = [
 		199, 200, 208, 210, 200, 207, 240, 269, 260, 263
 	];
-
-	const count = countDepthIncreased(depths);
-
+	const count = countPairsIncreasing(depths);
 	assert(count == 7, format!"Expected 7, received: %s"(count));
+}
+
+uint countTripletsIncreasing(in uint[] depths)
+{
+	return depths.tripletwise
+		.map!(a => a[0] + a[1] + a[2])
+		.array
+		.pairwise
+		.map!(a => a[0] < a[1])
+		.map!(to!uint)
+		.reduce!"a + b";
+}
+
+unittest
+{
+	const uint[] depths = [
+		199, 200, 208, 210, 200, 207, 240, 269, 260, 263
+	];
+	const count = countTripletsIncreasing(depths);
+	assert(count == 5, format!"Expected 5, received: %s"(count));
+}
+
+auto pairwise(T)(T[] a)
+{
+	return zip(a, a.dropOne);
+}
+
+auto tripletwise(T)(T[] a)
+{
+	return zip(a, a.dropOne, a.dropOne.dropOne);
 }
