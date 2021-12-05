11dd608d6213e911397c8097e8c9ab76f8c9f46f
Joshua Hodkinson <joshua.hodkinson@thoughtworks.com>
2021-12-05 18:25:47 +1000

    [D05B]

--- 2021/day05.d
+++ 2021/day05.d
@@ -1,6 +1,7 @@
 import std.algorithm;
 import std.array;
 import std.conv;
+import std.math;
 import std.regex;
 import std.stdio;
 import std.typecons;
@@ -14,7 +15,8 @@ void main()
 		.map!(ints => Segment([ints[0], ints[1]], [ints[2], ints[3]]))
 		.array;
 
-	segments.buildField.countOverlaps.writeln;
+	segments.buildField(false).countOverlaps.writeln;
+	segments.buildField(true).countOverlaps.writeln;
 }
 
 alias Point = int[2];
@@ -23,16 +25,32 @@ alias Segment = Tuple!(Point, "start", Point, "end");
 Point[] getPoints(in Segment segment)
 {
 	const x1 = min(segment.start[0], segment.end[0]);
-	const x2 = max(segment.start[0], segment.end[0]) + 1;
+	const x2 = max(segment.start[0], segment.end[0]);
 	const y1 = min(segment.start[1], segment.end[1]);
-	const y2 = max(segment.start[1], segment.end[1]) + 1;
+	const y2 = max(segment.start[1], segment.end[1]);
 
 	Point[] points;
-	foreach (col; x1 .. x2)
+
+	if (x1 == x2 || y1 == y2)
 	{
-		foreach (row; y1 .. y2)
+		foreach (col; x1 .. x2 + 1)
 		{
-			points ~= [col, row];
+			foreach (row; y1 .. y2 + 1)
+			{
+				points ~= [
+					col, row
+				];
+			}
+		}
+	}
+	else if ((y1 - x1).abs == (y2 - x2).abs)
+	{
+		const steps = x2 - x1;
+		const xSign = segment.start[0] > segment.end[0] ? -1 : 1;
+		const ySign = segment.start[1] > segment.end[1] ? -1 : 1;
+		foreach (i; 0 .. steps + 1)
+		{
+			points ~= [segment.start[0] + i * xSign, segment.start[1] + i * ySign];
 		}
 	}
 
@@ -51,9 +69,20 @@ unittest
 	assert(segment2.getPoints.equal([
 				[7, 7], [8, 7], [9, 7]
 			]));
+
+	const segment3 = Segment([1, 1], [3, 3]);
+	const segment4 = Segment([9, 7], [7, 9]);
+
+	assert(segment3.getPoints.equal([
+				[1, 1], [2, 2], [3, 3]
+			]));
+
+	assert(segment4.getPoints.equal([
+				[9, 7], [8, 8], [7, 9]
+			]));
 }
 
-int[][] buildField(in Segment[] segments)
+int[][] buildField(in Segment[] segments, bool allowDiagonals)
 {
 	int cols;
 	int rows;
@@ -66,7 +95,8 @@ int[][] buildField(in Segment[] segments)
 	int[][] field = new int[][](cols + 1, rows + 1);
 	foreach (Segment segment; segments)
 	{
-		if (segment.start[0] != segment.end[0] && segment.start[1] != segment.end[1])
+		if (!allowDiagonals && segment.start[0] != segment.end[0] && segment.start[1] != segment
+			.end[1])
 			continue;
 
 		foreach (Point point; segment.getPoints)
@@ -109,7 +139,9 @@ unittest
 		Segment([5, 5], [8, 2]),
 	];
 
-	const field = buildField(segments);
+	assert(buildField(segments, false).countOverlaps == 5);
 
-	assert(field.countOverlaps == 5);
+	const field = buildField(segments, true);
+
+	assert(field.countOverlaps == 12);
 }
