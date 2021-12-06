3a6b0cf311da4daa08d4368c2cee673588f201ce
Joshua Hodkinson <joshua.hodkinson@thoughtworks.com>
2021-12-06 18:01:57 +1000

    [D06B]

--- 2021/day06.d
+++ 2021/day06.d
@@ -6,77 +6,79 @@ import std.stdio;
 
 void main()
 {
-	File("day06.in", "r")
+	const seed = File("day06.in", "r")
 		.readln
 		.split(',')
 		.map!(to!int)
-		.array
-		.sim(80)
-		.length
+		.array;
+
+	seed.sim(80)
+		.writeln;
+	seed.sim(256)
 		.writeln;
 }
 
-int[] sim(in int[] seed, int days)
+long sim(in int[] seed, int days)
 {
-	int[] result = seed.dup;
+	long[] result = new long[](9);
 
-	if (days == 0)
+	foreach (s; seed)
 	{
-		return result;
+		++result[s];
 	}
 
-	foreach (idx, fish; seed)
+	foreach (day; 0 .. days)
 	{
-		if (fish == 0)
+		foreach (idx, value; result.dup)
 		{
-			result ~= 8;
-			result[idx] = 6;
-		}
-		else
-		{
-			--result[idx];
+			if (value > 0)
+			{
+				result[idx] -= value;
+
+				if (idx == 0)
+				{
+					result[8] += value;
+					result[6] += value;
+				}
+				else
+				{
+					result[idx - 1] += value;
+				}
+			}
 		}
 	}
 
-	return result.sim(days - 1);
+	long sum = 0;
+	foreach (value; result)
+	{
+		sum += value;
+	}
+
+	return sum;
 }
 
 unittest
 {
 	int[] seed = [3, 4, 3, 1, 2];
 
-	assert(sim(seed, 1).equal([2, 3, 2, 0, 1]));
-	assert(sim(seed, 2).equal([1, 2, 1, 6, 0, 8]));
-	assert(sim(seed, 3).equal([0, 1, 0, 5, 6, 7, 8]));
-	assert(sim(seed, 4).equal([6, 0, 6, 4, 5, 6, 7, 8, 8]));
-	assert(sim(seed, 5).equal([5, 6, 5, 3, 4, 5, 6, 7, 7, 8]));
-	assert(sim(seed, 6).equal([4, 5, 4, 2, 3, 4, 5, 6, 6, 7]));
-	assert(sim(seed, 7).equal([3, 4, 3, 1, 2, 3, 4, 5, 5, 6]));
-	assert(sim(seed, 8).equal([2, 3, 2, 0, 1, 2, 3, 4, 4, 5]));
-	assert(sim(seed, 9).equal([1, 2, 1, 6, 0, 1, 2, 3, 3, 4, 8]));
-	assert(sim(seed, 10).equal([0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 7, 8]));
-	assert(sim(seed, 11).equal([6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 7, 8, 8, 8]));
-	assert(sim(seed, 12).equal([
-			5, 6, 5, 3, 4, 5, 6, 0, 0, 1, 5, 6, 7, 7, 7, 8, 8
-		]));
-	assert(sim(seed, 13).equal([
-			4, 5, 4, 2, 3, 4, 5, 6, 6, 0, 4, 5, 6, 6, 6, 7, 7, 8, 8
-		]));
-	assert(sim(seed, 14).equal([
-			3, 4, 3, 1, 2, 3, 4, 5, 5, 6, 3, 4, 5, 5, 5, 6, 6, 7, 7, 8
-		]));
-	assert(sim(seed, 15).equal([
-			2, 3, 2, 0, 1, 2, 3, 4, 4, 5, 2, 3, 4, 4, 4, 5, 5, 6, 6, 7
-		]));
-	assert(sim(seed, 16).equal([
-			1, 2, 1, 6, 0, 1, 2, 3, 3, 4, 1, 2, 3, 3, 3, 4, 4, 5, 5, 6, 8
-		]));
-	assert(sim(seed, 17).equal([
-			0, 1, 0, 5, 6, 0, 1, 2, 2, 3, 0, 1, 2, 2, 2, 3, 3, 4, 4, 5, 7, 8
-		]));
-	assert(sim(seed, 18).equal([
-			6, 0, 6, 4, 5, 6, 0, 1, 1, 2, 6, 0, 1, 1, 1, 2, 2, 3, 3, 4, 6, 7, 8, 8,
-			8, 8
-		]));
-	assert(sim(seed, 18).length == 26);
+	assert(sim(seed, 1) == 5);
+	assert(sim(seed, 2) == 6);
+	assert(sim(seed, 3) == 7);
+	assert(sim(seed, 4) == 9);
+	assert(sim(seed, 5) == 10);
+	assert(sim(seed, 6) == 10);
+	assert(sim(seed, 7) == 10);
+	assert(sim(seed, 8) == 10);
+	assert(sim(seed, 9) == 11);
+	assert(sim(seed, 10) == 12);
+	assert(sim(seed, 11) == 15);
+	assert(sim(seed, 12) == 17);
+	assert(sim(seed, 13) == 19);
+	assert(sim(seed, 14) == 20);
+	assert(sim(seed, 15) == 20);
+	assert(sim(seed, 16) == 21);
+	assert(sim(seed, 17) == 22);
+	assert(sim(seed, 18) == 26);
+	assert(sim(seed, 80) == 5934);
+	assert(sim(seed, 256) == 26_984_457_539);
 }
