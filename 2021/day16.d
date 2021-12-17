1d3025caf90cf4d629a49657566c81e9e505727b
Joshua Hodkinson <joshua.hodkinson@thoughtworks.com>
2021-12-17 11:16:12 +1000

    [D16B]

--- 2021/day16.d
+++ 2021/day16.d
@@ -5,14 +5,17 @@ import std.array;
 import std.conv;
 import std.stdio;
 import std.string;
+import std.range;
 import std.typecons;
 
 void main()
 {
-	File("day16.in", "r")
+	auto packets = File("day16.in", "r")
 		.readln
-		.decode
-		.parseTransmission
+		.decode;
+
+	packets
+		.parsePacket
 		.writeln;
 }
 
@@ -65,90 +68,217 @@ unittest
 		], packets));
 }
 
-int parseTransmission(in ubyte[] packets)
+alias Result = Tuple!(ulong, "val", ulong, "ver");
+Result parsePacket(ref ubyte[] packets, uint depth = 0)
 {
-	int sum;
-	auto read = packets[0 .. $];
-	while (read.length)
+	ulong ver = packets[0] << 2 | packets[1] << 1 | packets[2];
+	packets = packets[3 .. $];
+
+	const type = packets[0] << 2 | packets[1] << 1 | packets[2];
+	packets = packets[3 .. $];
+
+	ulong result;
+	if (type == 4)
 	{
-		if (read.sum == 0)
+		const literal = parseLiteral(packets);
+		packets = packets[literal.length .. $];
+		result = literal.value;
+
+	}
+	else
+	{
+		const lengthType = packets[0];
+		packets = packets[1 .. $];
+
+		Result[] subResults;
+		if (lengthType)
 		{
-			break;
+			const count = packets[0 .. 11].intFromBits;
+			packets = packets[11 .. $];
+
+			foreach (i; 0 .. count)
+			{
+				subResults ~= parsePacket(packets, depth + 1);
+			}
+		}
+		else
+		{
+			const length = packets[0 .. 15].intFromBits;
+			packets = packets[15 .. $];
+
+			auto temp = packets[0 .. length];
+			while (temp.length)
+			{
+				subResults ~= parsePacket(temp, depth + 1);
+			}
+
+			packets = packets[length .. $];
 		}
 
-		sum += read[0] << 2 | read[1] << 1 | read[2];
-		read = read[3 .. $];
-		const type = read[0] << 2 | read[1] << 1 | read[2];
-		read = read[3 .. $];
-
-		switch (type)
+		final switch (type)
 		{
-		case 4:
-			const literal = parseLiteral(read);
-			read = read[literal.length .. $];
+		case 0:
+			result = 0;
+			foreach (r; subResults)
+			{
+				result += r.val;
+				ver += r.ver;
+			}
 			break;
 
-		default:
-			const lengthType = read[0];
-			read = read[1 .. $];
-
-			if (lengthType)
+		case 1:
+			result = 1;
+			foreach (r; subResults)
 			{
-				const count = read[0 .. 11].intFromBits;
-				read = read[11 .. $];
+				result *= r.val;
+				ver += r.ver;
 			}
-			else
+			break;
+		case 2:
+			result = ulong.max;
+			foreach (r; subResults)
 			{
-				const length = read[0 .. 15].intFromBits;
-				read = read[15 .. $];
+				result = min(result, r.val);
+				ver += r.ver;
 			}
+			break;
+		case 3:
+			result = ulong.min;
+			foreach (r; subResults)
+			{
+				result = max(result, r.val);
+				ver += r.ver;
+			}
+			break;
+		case 5:
+			assert(subResults.length == 2);
+			const p1 = subResults[0];
+			const p2 = subResults[1];
+			result = (p1.val > p2.val) ? 1 : 0;
 
+			ver += p1.ver + p2.ver;
+			break;
+		case 6:
+			assert(subResults.length == 2);
+			const p1 = subResults[0];
+			const p2 = subResults[1];
+			result = (p1.val < p2.val) ? 1 : 0;
+			ver += p1.ver + p2.ver;
+			break;
+		case 7:
+			assert(subResults.length == 2);
+			const p1 = subResults[0];
+			const p2 = subResults[1];
+			result = (p1.val == p2.val) ? 1 : 0;
+			ver += p1.ver + p2.ver;
 			break;
 		}
 	}
 
-	return sum;
+	if (depth == 0)
+	{
+		writeln;
+		assert(packets.count!"a > 0" == 0);
+	}
+
+	return Result(result, ver);
 }
 
 unittest
 {
-	const packets = "8A004A801A8002F478".decode;
-	const sum = parseTransmission(packets);
+	auto packets = "8A004A801A8002F478".decode;
+	const sum = parsePacket(packets)[1];
 	assert(sum == 16, format!"Expected %s, received: %s"(16, sum));
 }
 
 unittest
 {
-	const packets = "620080001611562C8802118E34".decode;
-	const sum = parseTransmission(packets);
+	auto packets = "620080001611562C8802118E34".decode;
+	const sum = parsePacket(packets)[1];
 	assert(sum == 12, format!"Expected %s, received: %s"(12, sum));
 }
 
 unittest
 {
-	const packets = "C0015000016115A2E0802F182340".decode;
-	const sum = parseTransmission(packets);
+	auto packets = "C0015000016115A2E0802F182340".decode;
+	const sum = parsePacket(packets)[1];
 	assert(sum == 23, format!"Expected %s, received: %s"(23, sum));
 }
 
 unittest
 {
-	const packets = "A0016C880162017C3686B18A3D4780".decode;
-	const sum = parseTransmission(packets);
+	auto packets = "A0016C880162017C3686B18A3D4780".decode;
+	const sum = parsePacket(packets)[1];
 	assert(sum == 31, format!"Expected %s, received: %s"(31, sum));
 }
 
-uint intFromBits(in ubyte[] bits)
+unittest
 {
-	uint value;
+	auto packets = "C200B40A82".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 3, format!"Expected %s, received: %s"(3, val));
+}
+
+unittest
+{
+	auto packets = "04005AC33890".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 54, format!"Expected %s, received: %s"(54, val));
+}
+
+unittest
+{
+	auto packets = "880086C3E88112".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 7, format!"Expected %s, received: %s"(7, val));
+}
+
+unittest
+{
+	auto packets = "CE00C43D881120".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 9, format!"Expected %s, received: %s"(9, val));
+}
+
+unittest
+{
+	auto packets = "D8005AC2A8F0".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 1, format!"Expected %s, received: %s"(1, val));
+}
+
+unittest
+{
+	auto packets = "F600BC2D8F".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 0, format!"Expected %s, received: %s"(0, val));
+}
+
+unittest
+{
+	auto packets = "9C005AC2F8F0".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 0, format!"Expected %s, received: %s"(0, val));
+}
+
+unittest
+{
+	auto packets = "9C0141080250320F1802104A08".decode;
+	const val = parsePacket(packets).val;
+	assert(val == 1, format!"Expected %s, received: %s"(1, val));
+}
+
+ulong intFromBits(in ubyte[] bits)
+{
+	ulong value;
 	foreach (count, bit; bits)
 	{
-		value |= bit << (bits.length - 1 - count);
+		value = (value << 1) + bit;
 	}
 	return value;
 }
 
-alias Literal = Tuple!(int, "value", int, "length");
+alias Literal = Tuple!(ulong, "value", int, "length");
 Literal parseLiteral(in ubyte[] packets)
 {
 	ubyte[] bits;
