81de125dc99fda72d719846dde10025def468dd6
Joshua Hodkinson <joshua.hodkinson.42@gmail.com>
2022-05-27 14:58:48 +1000

    Update

---
+++ 2021/day17.d
@@ -0,0 +1,18 @@
+import std.algorithm;
+import std.array;
+import std.conv;
+import std.stdio;
+import std.string;
+import std.typecons;
+
+const input = "target area: x=206..250, y=-105..-57";
+
+void main()
+{
+	File("day17.in", "r");
+}
+
+unittest
+{
+	assert(true);
+}
